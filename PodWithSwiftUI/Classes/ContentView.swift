cac53d2a90154c0bc586267687b28a3f2a2bc1c2
mabayon <mabayon@student.42.fr>
2022-08-18 18:11:23 +0200

    setup

---
+++ PodWithSwiftUI/Classes/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Project
+//
+//  Created by Mathieu Bayon on 18/08/2022.
+//
+
+import SwiftUI
+
+public struct ContentView: View {
+    public init() {}
+
+    public var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
